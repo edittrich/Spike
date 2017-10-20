257128dfb232a127994b3b15a80dbedc916ebb5e
Erik Dittrich <erik.dittrich@gmail.com>
2017-10-20 12:46:16 +0200

    Initial Commit

--- SpikeSpigot/src/main/java/de/edittrich/spike/spigot/SpigotSpikePlugin.java
+++ SpikeSpigot/src/main/java/de/edittrich/spike/spigot/SpigotSpikePlugin.java
@@ -1,10 +1,13 @@
 package de.edittrich.spike.spigot;
 
+import java.util.logging.Logger;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpigotSpikePlugin extends JavaPlugin {
 	private final FileConfiguration config = getConfig();
+	private final Logger logger = getLogger();
 
 	public void onEnable() {
 		config.addDefault("youAreAwesome", true);
@@ -12,7 +15,7 @@ public class SpigotSpikePlugin extends JavaPlugin {
 		config.options().copyDefaults(true);
 		saveConfig();
 
-		getLogger().info("onEnable has been invoked with youAreAwesome: " + config.getBoolean("youAreAwesome"));
+		logger.info("onEnable has been invoked with youAreAwesome: " + config.getBoolean("youAreAwesome"));
 
 		this.getCommand("basic").setExecutor(new SpigotSpikeCommandExecutorBasic(this));
 		this.getCommand("cube").setExecutor(new SpigotSpikeCommandExecutorCube(this));
